98fb0e26ff2d89cc0178de74d45f710c0d69cdff
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-06-05 11:33:10 -0500

    updated code

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const router =  require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercise: body } },
+        //"runValidators" will ensure new exercises meet our schema requirements
+        { new: true, renValidators: true }
+    )
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
